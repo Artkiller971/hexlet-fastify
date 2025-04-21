29d9330f40b076caa4b38ba571ecf9c8fb733f01
Anton <koshevnikod@gmail.com>
2025-04-21 18:43:59 +0300

    dynamic parameters

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import fastify from 'fastify';
 
+const state = {
+  users: [
+    {
+      id: 1,
+      name: 'user',
+    },
+  ],
+};
+
 const app = fastify();
 const port = 3000;
 
@@ -13,8 +22,29 @@ app.post('/users', (req, res) => {
 
 app.get('/hello', (req, res) => {
   const name = req.query.name || 'World';
-
   res.send(`Hello ${name}!`);
+});
+
+app.get('/courses/:id', (req, res) => {
+  res.send(`Course ID: ${req.params.id}`);
+});
+
+app.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const user = state.users.find((user) => user.id === parseInt(id));
+  if (!user) {
+    res.code(404).send({ message: 'User not found' });
+  } else {
+    res.send(user);
+  }
+});
+
+app.get('/courses/:courseId/lessons/:id', (req, res) => {
+  res.send(`Course ID: ${req.params.courseId}; Lesson ID: ${req.params.id}`);
+});
+
+app.get('/users/:id/post/:postId', (req, res) => {
+  res.send(`User ID: ${req.params.id}; Post ID: ${req.params.postId}`);
 })
 
 app.listen({ port }, () => {
